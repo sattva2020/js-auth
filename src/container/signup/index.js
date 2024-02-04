3ca9694de4871beeb2a7f7d1a2b8af13e714825d
sattva2020 <ruslan.griban@gmail.com>
2024-02-04 14:20:03 +0200

    js-auth part-2

--- src/container/signup/index.js
+++ src/container/signup/index.js
@@ -1,4 +1,8 @@
-import { Form } from '../../script/form'
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
 
 class SignupForm extends Form {
   FIELD_NAME = {
@@ -21,14 +25,91 @@ class SignupForm extends Form {
     ROLE: 'Ви не обрали роль',
   }
 
-   validate = (name, value) => {
-    return true
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value))) {
+        return this.FIELD_ERROR.EMAIL
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD) {
+      if (!REG_EXP_PASSWORD.test(String(value))) {
+        return this.FIELD_ERROR.PASSWORD
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
+      if (
+        String(value) !==
+        this.value[this.FIELD_NAME.PASSWORD]
+      ) {
+        return this.FIELD_ERROR.PASSWORD_AGAIN
+      }
+    }
+
+    if (name === this.FIELD_NAME.ROLE) {
+      if (isNaN(value)) {
+        return this.FIELD_ERROR.ROLE
+      }
+    }
+
+    if (name === this.FIELD_NAME.IS_CONFIRM) {
+      if (Boolean(value) !== true) {
+        return this.FIELD_ERROR.NOT_CONFIRM
+      }
+    }
   }
 
-  submit = () => {
-    console.log(this.value)
+  submit = async () => {
+    if (this.disabled === true) {
+      this.validateAll()
+    } else {
+      console.log(this.value)
+
+    this.setAlert('progress', 'Завантаження...')  
+
+    try {
+      const res = await fetch('/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: this.convertData(),
+      })
+     
+      const data = await res.json()
+
+      if(res.ok) {
+        this.satAlert('success', data.message)
+      } else {
+        this.setAlert('error', data.message)
+      }
+
+    } catch (error) {
+      this.setAlert('error', error.message)
+    }
+
+    }
   }
 
+  convertData = () => {
+    return JSON.stringify({
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
+      [this.FIELD_NAME.PASSWORD]:
+        this.value[this.FIELD_NAME.PASSWORD],
+      [this.FIELD_NAME.ROLE]:
+        this.value[this.FIELD_NAME.ROLE],
+    })
+  }
 }
 
 window.signupForm = new SignupForm()
