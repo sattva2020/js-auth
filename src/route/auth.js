48f5e685628175a922be2a6b7f66f8bd5ea25a04
sattva2020 <ruslan.griban@gmail.com>
2024-02-05 15:18:52 +0200

    js-auth upd part-3

--- src/route/auth.js
+++ src/route/auth.js
@@ -4,6 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 const { User } = require('../class/user')
+const { Confirm } = require('../class/confirm')
 
 User.create({
   email: 'user@mail.com',
@@ -81,12 +82,21 @@ router.post('/signup', function (req, res) {
   }
 
   try {
-      User.create({ email, password, role })
+    const user = User.getByEmail(email)
 
-     return res.status(200).json({
-       message: 'Користувач успішно зареєстрованний',
-     })
-     
+    if (!user) {
+      return res.status(400).json({
+        message: 'Користувач з таким email не існує',
+      })
+    }
+
+      Confirm.create(email)
+
+      return res.status(200).json({
+        message: 'Користувач успішно зареєстрований',
+        session,
+      })
+
       } catch (err) {
         return res.status(400).json({
           message: 'Помилка створення користувача',
@@ -118,6 +128,9 @@ router.get('/recovery', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
+
+// ================================================================
+
 
 router.post('/recovery', function (req, res) {
   const { email } = req.body
@@ -151,6 +164,77 @@ router.post('/recovery', function (req, res) {
   }
 })
 
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/recovery-confirm', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  return res.render('recovery-confirm', {
+    // вказуємо назву контейнера
+    name: 'recovery-confirm',
+    // вказуємо назву компонентів
+    component: ['back-button', 'field', 'field-password'],
+
+    // вказуємо назву сторінки
+    title: 'Recovery confirm page',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {},
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/recovery-confirm', function (req, res) {
+  const { password, code } = req.body
+
+  console.log(password, code)
+
+  if (!code || !password) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    const email = Confirm.getData(Number(code))
+
+    if (!email) {
+      return res.status(400).json({
+        message: 'Код не існує',
+      })
+    }
+
+    const user = User.getByEmail(email)
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'Користувач з таким email не існує',
+      })
+    }
+
+    user.password = password
+
+    console.log(user)
+
+    const session = Session.create(user)
+
+    return res.status(200).json({
+      message: 'Пароль змінено',
+      session,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
+})
+
+// ================================================================
 
 
 // Підключаємо роутер до бек-енду
